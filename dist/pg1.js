8799f774dfcd611ee7d69456320100e76c1d5b51
triple93r <56282248+triple93r@users.noreply.github.com>
2023-10-24 19:41:09 +0530

    init55

---
+++ dist/pg1.js
@@ -0,0 +1,14 @@
+"use strict";
+let age = 30;
+console.log(age);
+let uname = "John";
+console.log(uname);
+let isStudent = true;
+console.log(isStudent);
+let numbers = [1, 2, 3, 4];
+console.log(numbers);
+let person = ["John", 30];
+console.log(person);
+function alt() {
+    alert('Hitted Alt');
+}
